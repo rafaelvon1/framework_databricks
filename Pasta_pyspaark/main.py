e2ec252848a389a7ba9a14a230c37d914a291abd
Rafael de sousa <161399465+rafaelvon1@users.noreply.github.com>
2026-02-04 14:41:11 -0300

    Update main.py

--- Pasta_pyspaark/main.py
+++ Pasta_pyspaark/main.py
@@ -1,33 +1,8 @@
-df = spark.read.format("csv") \
-    .option("header", "true") \
-    .option("inferSchema", "true") \
-    .load("/FileStore/tables/clientes.csv")
-
-display(df)
-from pyspark.sql.functions import col
-
-df_limpo = df.dropna(subset=["id", "nome", "idade", "salario"])
-
-display(df_limpo)
-from pyspark.sql.functions import when, upper
-
-df_transformado = df_limpo \
-    .withColumn("nome_maiusculo", upper(col("nome"))) \
-    .withColumn(
-        "faixa_etaria",
-        when(col("idade") < 18, "menor")
-        .when(col("idade") <= 60, "adulto")
-        .otherwise("idoso")
-    )
-
-display(df_transformado)
-df_filtrado = df_transformado.filter(col("salario") > 3000)
-
-display(df_filtrado)
-
-df_agregado = df_filtrado.groupBy("faixa_etaria") \
-    .count() \
-    .withColumnRenamed("count", "qtd_clientes")
-
-
-
+SELECT
+  ws_cast(
+    id_produto AS INT,
+    preco AS DECIMAL(10,2),
+    quantidade AS INT,
+    data_venda AS DATE
+  )
+FROM dataset.silver_git.atacadao;
